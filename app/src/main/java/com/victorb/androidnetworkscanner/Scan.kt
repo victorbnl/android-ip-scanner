615c5b46ee7adce45ebfa8106bb0d85ae12ffab1
Victor B <victor.bonnelle@protonmail.com>
2021-01-13 22:37:27 +0100

    Better scan performances by fixing GlobalScope problem

--- app/src/main/java/com/victorb/androidnetworkscanner/Scan.kt
+++ app/src/main/java/com/victorb/androidnetworkscanner/Scan.kt
@@ -3,9 +3,7 @@ package com.victorb.androidnetworkscanner
 import android.app.Activity
 import android.view.View
 import android.widget.ProgressBar
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.net.Inet4Address
 import java.net.InetAddress
 import java.net.InterfaceAddress
@@ -17,9 +15,15 @@ class Scanner(private val ip: Int,
               private val progressBar: ProgressBar,
               private val activity: Activity) {
 
+    // The timeout of the network request
     private val timeout: Int = 2000
+
+    
     private lateinit var scanningJob: Job
+
+    private val checkingJobsScope = CoroutineScope(Dispatchers.IO)
     private val checkingJobs: MutableList<Job> = arrayListOf()
+
     private var networkPrefixLength: Int = 24
 
     init {
@@ -38,12 +42,18 @@ class Scanner(private val ip: Int,
 
         // Start scan
         scanningJob = GlobalScope.launch {
+            // Start checking all ips
             checkIps(ip, networkPrefixLength)
-        }
 
-        // Hide progress bar when finished
-        scanningJob.invokeOnCompletion {
-            setProgressBarVisibility(View.GONE)
+            // Wait for all the jobs to finish
+            for (job in checkingJobs) {
+                job.join()
+            }
+
+            // Hide progress bar when finished
+            scanningJob.invokeOnCompletion {
+                setProgressBarVisibility(View.GONE)
+            }
         }
     }
 
@@ -82,26 +92,34 @@ class Scanner(private val ip: Int,
         }
     }
 
-    private fun checkIp(ip: Int) {
-        checkingJobs.add(GlobalScope.launch {
-            val inetAddress: InetAddress = InetAddress.getByAddress(ipToBytes(ip))
-            if (inetAddress.isReachable(timeout)) {
-                resultsList.add(Device(ipToString(ip), if (ip == this@Scanner.ip) "Your phone" else inetAddress.hostName.split(".")[0]))
-                activity.runOnUiThread {
-                    resultsAdapter.notifyDataSetChanged()
-                }
+    // Start a coroutine which checks the IP in the checkingJobsScope scope
+    // because GlobalScope doesn't work as expected
+    // https://elizarov.medium.com/the-reason-to-avoid-globalscope-835337445abc
+    private fun checkIp(ip: Int) = checkingJobsScope.launch {
+        // Define the IP address object
+        val inetAddress: InetAddress = InetAddress.getByAddress(ipToBytes(ip))
+        // Check if it's reachable
+        if (inetAddress.isReachable(timeout)) {
+            // Add the device in the results list with its ip and hostname
+            resultsList.add(Device(ipToString(ip), if (ip == this@Scanner.ip) "Your phone" else inetAddress.hostName.split(".")[0]))
+            // Notify the RecyclerView the list changed
+            activity.runOnUiThread {
+                resultsAdapter.notifyDataSetChanged()
             }
-        })
+        }
     }
 
+    // Show or hide the progress bar
     private fun setProgressBarVisibility(int: Int) {
         activity.runOnUiThread {
             progressBar.visibility = int
         }
     }
 
+    // Reserved for a future change in the algorithm
     private fun reverseIpBytes(ip: Int): Int = ((ip and 0xff) shl 24) + ((ip and 0xff) shl 16) + ((ip and 0xff) shl 8) + (ip and 0xff)
 
+    // Converts the IP to a human-readable string
     private fun ipToString(ip: Int) = String.format(
         "%d.%d.%d.%d",
         (ip and 0xff),
@@ -110,6 +128,7 @@ class Scanner(private val ip: Int,
         (ip shr 24 and 0xff)
     )
 
+    // Converts the ip to a four bytes array
     private fun ipToBytes(ip: Int) = arrayOf(
         (ip and 0xff).toByte(),
         (ip shr 8 and 0xff).toByte(),
